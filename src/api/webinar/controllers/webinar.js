78f55265bd6f5edea61f94a9c6bd550a208f39eb
Tom Groombridge <tom.groombridge@zopa.com>
2023-01-23 12:20:50 +0000

    add endpoint for showing users upcoming webinars

--- src/api/webinar/controllers/webinar.js
+++ src/api/webinar/controllers/webinar.js
@@ -6,4 +6,32 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::webinar.webinar');
+module.exports = createCoreController('api::webinar.webinar', ({ strapi }) => ({
+  async users_upcoming_events(ctx) {
+    if (!ctx.state.isAuthenticated) {
+      return ctx.unauthorized(['You are not authorised'], []);
+    }
+
+    const user = await strapi.entityService.findOne(
+      'plugin::users-permissions.user',
+      ctx.state.user.id,
+      { populate: ['webinars'] }
+    );
+
+    const date = new Date();
+    const iso = date.toISOString();
+
+    const upcoming_webinars = user.webinars.filter((webinar) => {
+      const webinar_time = Date.parse(webinar.date + 'T' + webinar.time);
+      const current_time = Date.parse(iso);
+      return webinar_time > current_time;
+    })
+
+    const sanitizedEntity = await this.sanitizeOutput(upcoming_webinars, ctx);
+
+    return this.transformResponse(sanitizedEntity);
+    // console.log('upcoming', upcoming_webinars)
+
+    // return upcoming_webinars
+  }
+}));
---
+++ src/api/webinar/routes/custom-webinar.js
@@ -0,0 +1,9 @@
+module.exports = {
+  routes: [
+    {
+      method: "GET",
+      path: "/users/webinars",
+      handler: "webinar.users_upcoming_events",
+    }
+  ],
+}
