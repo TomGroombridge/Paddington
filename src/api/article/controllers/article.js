438c0afe905aa3d7c23aea56c2e204c3b32395e3
Tom Groombridge <tom.groombridge@zopa.com>
2023-01-23 22:00:29 +0000

    fetch articles by slug

--- src/api/article/controllers/article.js
+++ src/api/article/controllers/article.js
@@ -1,9 +1,24 @@
-'use strict';
+"use strict";
 
 /**
- * article controller
+ *  post controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::article.article');
+module.exports = createCoreController("api::article.article", ({ strapi }) => ({
+  async findOne(ctx) {
+    const { slug } = ctx.params;
+
+    const query = {
+      filters: { slug },
+      ...ctx.query,
+    };
+
+    const article = await strapi.entityService.findMany("api::article.article", query);
+
+    const sanitizedEntity = await this.sanitizeOutput(article);
+
+    return this.transformResponse(sanitizedEntity[0]);
+  },
+}));
