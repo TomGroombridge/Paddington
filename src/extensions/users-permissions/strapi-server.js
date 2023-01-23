10368e6bb38b04074b55557260d8230e94b4f852
Tom Groombridge <tom.groombridge@zopa.com>
2023-01-23 11:20:20 +0000

    add endpoints for attending and unattending a event

--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -18,10 +18,91 @@ module.exports = (plugin) => {
     return user
   }
 
+  plugin.controllers.user.joinEvent = async (ctx) => {
+    if (!ctx.state.user) {
+      return ctx.unauthorized();
+    }
+
+    const { webinarId } = ctx.request.body
+
+    if (!webinarId) {
+      return ctx.badRequest('no webinar supplied')
+    }
+
+    // maybe look at checking the webinar is not in the past 
+    // check the user has not already signed up to the webinar
+
+    const user = await strapi.entityService.findOne(
+      'plugin::users-permissions.user',
+      ctx.state.user.id,
+      { populate: ['webinars'] }
+    );
+
+    const webinar = await strapi.entityService.findOne(
+      'api::webinar.webinar',
+      webinarId
+    )
+
+    const newData = [...user.webinars, webinar]
+
+    await strapi.entityService.update(
+      "plugin::users-permissions.user",
+      ctx.state.user.id,
+      {
+        data: {
+          webinars: newData
+        }
+      }
+    );
+
+    return user
+  }
+
+  plugin.controllers.user.unattendEvent = async (ctx) => {
+    if (!ctx.state.user) {
+      return ctx.unauthorized();
+    }
+
+    const { webinarId } = ctx.request.body
+
+    if (!webinarId) {
+      return ctx.badRequest('no webinar supplied')
+    }
+
+    const user = await strapi.entityService.findOne(
+      'plugin::users-permissions.user',
+      ctx.state.user.id,
+      { populate: ['webinars'] }
+    );
+
+    const newData = user.webinars.filter((webinar) => webinar.id !== webinarId);
+
+    await strapi.entityService.update(
+      "plugin::users-permissions.user",
+      ctx.state.user.id,
+      {
+        data: {
+          webinars: newData
+        }
+      }
+    );
+
+    return user
+
+  }
+
   plugin.routes['content-api'].routes.push({
     method: 'PUT',
     path: '/users/onboard',
     handler: 'user.onboard'
+  }, {
+    method: 'POST',
+    path: '/users/events/signup',
+    handler: 'user.joinEvent'
+  }, {
+    method: 'POST',
+    path: '/users/events/unattend',
+    handler: 'user.unattendEvent'
   });
 
   return plugin;
