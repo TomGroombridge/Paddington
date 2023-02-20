ab2b5788c7859e6f591394360619e569ea0d508d
Tom Groombridge <tom.groombridge@zopa.com>
2023-02-20 10:17:08 +0200

    add mobile field

--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -1,18 +1,18 @@
 module.exports = (plugin) => {
   plugin.controllers.user.onboard = async (ctx) => {
-    const { firstName, lastName, occupation } = ctx.request.body;
+    const { firstName, lastName, occupation, mobile } = ctx.request.body;
 
     if (!firstName || !lastName || !occupation) {
-      return ctx.badRequest('field is missing', { firstName, lastName, occupation })
+      return ctx.badRequest('field is missing', { firstName, lastName, occupation, mobile })
     }
     const user = await strapi.entityService.update('plugin::users-permissions.user', ctx.state.user.id, {
       data: {
         firstName,
         lastName,
         occupation,
-
+        mobile
       },
-      fields: ['firstName', 'lastName', 'occupation']
+      fields: ['firstName', 'lastName', 'occupation', 'mobile']
     });
 
     return user
