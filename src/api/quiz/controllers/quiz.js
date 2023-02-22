0064dc0a1ec92dcefd34bcc955a14469b0c530c5
Tom Groombridge <tom.groombridge@zopa.com>
2023-02-22 18:22:03 +0200

    add quiz slug for fetching

--- src/api/quiz/controllers/quiz.js
+++ src/api/quiz/controllers/quiz.js
@@ -6,4 +6,21 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::quiz.quiz');
+module.exports = createCoreController('api::quiz.quiz', ({ strapi }) => ({
+
+  async findOne(ctx) {
+    const { slug } = ctx.params;
+
+    const query = {
+      filters: { slug },
+      ...ctx.query,
+    };
+
+    const webinar = await strapi.entityService.findMany("api::quiz.quiz", query);
+
+    const sanitizedEntity = await this.sanitizeOutput(webinar);
+
+    return this.transformResponse(sanitizedEntity[0]);
+  },
+}));
+
